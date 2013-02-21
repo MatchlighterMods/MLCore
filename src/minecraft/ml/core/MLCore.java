447cc24324bd4ed0a1ab2f21c9050feb5da16ecd
Matchlighter <ml@matchlighter.net>
2013-02-20 21:29:12 -0700

    Added build.xml and mcmodinfo

---
+++ src/minecraft/ml/core/MLCore.java
@@ -0,0 +1,11 @@
+package ml.core;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+
+@Mod(modid="MLCore", name="MLCore", version="0.2")
+public class MLCore {
+	
+	@Instance("MLCore")
+	public MLCore instance;
+}
