a019dab392fe0d4f13d11e52ee38876be2cc5864
Matchlighter <ml@matchlighter.net>
2013-04-02 15:35:16 -0600

    (Mostly) Finished OBJImporting.

--- src/minecraft/ml/core/lib/RenderLib.java
+++ src/minecraft/ml/core/lib/RenderLib.java
@@ -12,77 +12,144 @@ public class RenderLib {
 
 	public static RenderItem itemRenderer = new RenderItem();
 	public static int zLevel = 0;
-	
+
 	public static void drawTexturedModalRect(int x, int y, int u, int v, int width, int height)
-    {
-        float var7 = 0.00390625F;
-        float var8 = 0.00390625F;
-        Tessellator var9 = Tessellator.instance;
-        var9.startDrawingQuads();
-        var9.addVertexWithUV((double)(x + 0), (double)(y + height), (double)zLevel, (double)((float)(u + 0) * var7), (double)((float)(v + height) * var8));
-        var9.addVertexWithUV((double)(x + width), (double)(y + height), (double)zLevel, (double)((float)(u + width) * var7), (double)((float)(v + height) * var8));
-        var9.addVertexWithUV((double)(x + width), (double)(y + 0), (double)zLevel, (double)((float)(u + width) * var7), (double)((float)(v + 0) * var8));
-        var9.addVertexWithUV((double)(x + 0), (double)(y + 0), (double)zLevel, (double)((float)(u + 0) * var7), (double)((float)(v + 0) * var8));
-        var9.draw();
-    }
-	
-    public static void drawGradientRect(int par1, int par2, int par3, int par4, int par5, int par6)
-    {
-        float var7 = (float)(par5 >> 24 & 255) / 255.0F;
-        float var8 = (float)(par5 >> 16 & 255) / 255.0F;
-        float var9 = (float)(par5 >> 8 & 255) / 255.0F;
-        float var10 = (float)(par5 & 255) / 255.0F;
-        float var11 = (float)(par6 >> 24 & 255) / 255.0F;
-        float var12 = (float)(par6 >> 16 & 255) / 255.0F;
-        float var13 = (float)(par6 >> 8 & 255) / 255.0F;
-        float var14 = (float)(par6 & 255) / 255.0F;
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glDisable(GL11.GL_ALPHA_TEST);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glShadeModel(GL11.GL_SMOOTH);
-        Tessellator var15 = Tessellator.instance;
-        var15.startDrawingQuads();
-        var15.setColorRGBA_F(var8, var9, var10, var7);
-        var15.addVertex((double)par3, (double)par2, (double)zLevel);
-        var15.addVertex((double)par1, (double)par2, (double)zLevel);
-        var15.setColorRGBA_F(var12, var13, var14, var11);
-        var15.addVertex((double)par1, (double)par4, (double)zLevel);
-        var15.addVertex((double)par3, (double)par4, (double)zLevel);
-        var15.draw();
-        GL11.glShadeModel(GL11.GL_FLAT);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glEnable(GL11.GL_ALPHA_TEST);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-    }
-	
+	{
+		float var7 = 0.00390625F;
+		float var8 = 0.00390625F;
+		Tessellator var9 = Tessellator.instance;
+		var9.startDrawingQuads();
+		var9.addVertexWithUV((double)(x + 0), (double)(y + height), (double)zLevel, (double)((float)(u + 0) * var7), (double)((float)(v + height) * var8));
+		var9.addVertexWithUV((double)(x + width), (double)(y + height), (double)zLevel, (double)((float)(u + width) * var7), (double)((float)(v + height) * var8));
+		var9.addVertexWithUV((double)(x + width), (double)(y + 0), (double)zLevel, (double)((float)(u + width) * var7), (double)((float)(v + 0) * var8));
+		var9.addVertexWithUV((double)(x + 0), (double)(y + 0), (double)zLevel, (double)((float)(u + 0) * var7), (double)((float)(v + 0) * var8));
+		var9.draw();
+	}
+
+	public static void drawGradientRect(int par1, int par2, int par3, int par4, int par5, int par6)
+	{
+		float var7 = (float)(par5 >> 24 & 255) / 255.0F;
+		float var8 = (float)(par5 >> 16 & 255) / 255.0F;
+		float var9 = (float)(par5 >> 8 & 255) / 255.0F;
+		float var10 = (float)(par5 & 255) / 255.0F;
+		float var11 = (float)(par6 >> 24 & 255) / 255.0F;
+		float var12 = (float)(par6 >> 16 & 255) / 255.0F;
+		float var13 = (float)(par6 >> 8 & 255) / 255.0F;
+		float var14 = (float)(par6 & 255) / 255.0F;
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glShadeModel(GL11.GL_SMOOTH);
+		Tessellator var15 = Tessellator.instance;
+		var15.startDrawingQuads();
+		var15.setColorRGBA_F(var8, var9, var10, var7);
+		var15.addVertex((double)par3, (double)par2, (double)zLevel);
+		var15.addVertex((double)par1, (double)par2, (double)zLevel);
+		var15.setColorRGBA_F(var12, var13, var14, var11);
+		var15.addVertex((double)par1, (double)par4, (double)zLevel);
+		var15.addVertex((double)par3, (double)par4, (double)zLevel);
+		var15.draw();
+		GL11.glShadeModel(GL11.GL_FLAT);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_ALPHA_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+	}
+
 	public static void drawStackAt(Minecraft mc, int x, int y, ItemStack is){
 		GL11.glEnable(GL11.GL_LIGHTING);
-        itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, is, x, y);
-        itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, is, x, y);
-        GL11.glDisable(GL11.GL_LIGHTING);
+		itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, is, x, y);
+		itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, is, x, y);
+		GL11.glDisable(GL11.GL_LIGHTING);
 	}
-	
+
 	public static void drawSpecialStackAt(Minecraft mc, int x, int y, ItemStack is, String str){
 		GL11.glEnable(GL11.GL_LIGHTING);
 		ItemStack tis = is.copy();
 		tis.stackSize = 1;
-        itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, tis, x, y);
-        itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, tis, x, y);
-        
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        mc.fontRenderer.drawStringWithShadow(str, x + 19 - 2 - mc.fontRenderer.getStringWidth(str), y + 6 + 3, 16777215);
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        
-        GL11.glDisable(GL11.GL_LIGHTING);
+		itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.renderEngine, tis, x, y);
+		itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.renderEngine, tis, x, y);
+
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		mc.fontRenderer.drawStringWithShadow(str, x + 19 - 2 - mc.fontRenderer.getStringWidth(str), y + 6 + 3, 16777215);
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+
+		GL11.glDisable(GL11.GL_LIGHTING);
 	}
-	
+
 	public static void drawSlicedRect(int x, int y, int w, int h, int uBase, int vBase, int uW, int uH, int tBord, int rBord, int bBord, int lBord){
 		drawTexturedModalRect(x, y, uBase, vBase, w-rBord, h-bBord);
 		drawTexturedModalRect(x+lBord, y, uBase+uW-(w-lBord), vBase, w-lBord, h-bBord);
 		drawTexturedModalRect(x+lBord, y+tBord, uBase+uW-(w-lBord), vBase+uH-(h-tBord), w-lBord, h-tBord);
 		drawTexturedModalRect(x, y+tBord, uBase, vBase+uH-(h-tBord), w-rBord, h-tBord);
+	}
+
+	public static void renderItemIn2D(Tessellator tes, float umin, float vmin, float umax, float vmax, int szX, int szY, float thickness) { //Terribly Slow (WIP)
+		tes.startDrawingQuads();
+		tes.setNormal(0.0F, 0.0F, 1.0F);
+		tes.addVertexWithUV(0.0D, 0.0D, 0.0D, (double)umin, (double)vmax);
+		tes.addVertexWithUV(1.0D, 0.0D, 0.0D, (double)umax, (double)vmax);
+		tes.addVertexWithUV(1.0D, 1.0D, 0.0D, (double)umax, (double)vmin);
+		tes.addVertexWithUV(0.0D, 1.0D, 0.0D, (double)umin, (double)vmin);
+		tes.draw();
+
+		tes.startDrawingQuads();
+		tes.setNormal(0.0F, 0.0F, -1.0F);
+		tes.addVertexWithUV(0.0D, 1.0D, (double)(0.0F - thickness), (double)umin, (double)vmin);
+		tes.addVertexWithUV(1.0D, 1.0D, (double)(0.0F - thickness), (double)umax, (double)vmin);
+		tes.addVertexWithUV(1.0D, 0.0D, (double)(0.0F - thickness), (double)umax, (double)vmax);
+		tes.addVertexWithUV(0.0D, 0.0D, (double)(0.0F - thickness), (double)umin, (double)vmax);
+		tes.draw();
+
+		GL11.glPushMatrix();
+		GL11.glTranslatef(0, 0, -thickness);
+
+		float fx = 1/(float)szX;
+		float fy = 1/(float)szY;
+		GL11.glScalef(fx, fy, 1F);
+
+		for (float x=0; x<szX; x++){
+			float u = umin + (umax-umin) * (x/(float)szX);
+			for (float y=0; y<szY; y++){
+				float v = vmin + (vmax-vmin) * (y/(float)szY);
+				float ay = szY-y;
+				
+				tes.startDrawingQuads();
+				tes.setNormal(-1.0F, 0.0F, 0.0F);
+				tes.addVertexWithUV(x+1, ay, 0, u, v);
+				tes.addVertexWithUV(x+1, ay, thickness, u, v+fy);
+				tes.addVertexWithUV(x+1, ay-1, thickness, u+fx, v+fy);
+				tes.addVertexWithUV(x+1, ay-1, 0, u+fx, v);
+				tes.draw();
+				
+				tes.startDrawingQuads();
+				tes.setNormal(1.0F, 0F, 0.0F);
+				tes.addVertexWithUV(x, ay, 0, u, v);
+				tes.addVertexWithUV(x, ay-1, 0, u+fx, v);
+				tes.addVertexWithUV(x, ay-1, thickness, u+fx, v+fy);
+				tes.addVertexWithUV(x, ay, thickness, u, v+fy);
+				tes.draw();
+
+				tes.startDrawingQuads();
+				tes.setNormal(0.0F, 1.0F, 0.0F);
+				tes.addVertexWithUV(x, ay, 0, u, v);
+				tes.addVertexWithUV(x, ay, thickness, u, v+fy);
+				tes.addVertexWithUV(x+1, ay, thickness, u+fx, v+fy);
+				tes.addVertexWithUV(x+1, ay, 0, u+fx, v);
+				tes.draw();
+
+				tes.startDrawingQuads();
+				tes.setNormal(0.0F, -1.0F, 0.0F);
+				tes.addVertexWithUV(x, ay-1, 0, u, v);
+				tes.addVertexWithUV(x+1, ay-1, 0, u+fx, v);
+				tes.addVertexWithUV(x+1, ay-1, thickness, u+fx, v+fy);
+				tes.addVertexWithUV(x, ay-1, thickness, u, v+fy);
+				tes.draw();
+			}
+		}
+
+		GL11.glPopMatrix();
 	}
 }
--- src/minecraft/ml/core/model/ObjModel.java
+++ src/minecraft/ml/core/model/ObjModel.java
@@ -7,56 +7,152 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StreamTokenizer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.Tessellator;
+
 public class ObjModel {
-	
-	public static class ObjModelImporter{
+
+	private Map<String, List<uvVertex[]>> groups = new HashMap<String, List<uvVertex[]>>();
+	private Map<String, Integer> cpldGroups = new HashMap<String, Integer>();
+
+	public ObjModel(ObjModelImporter objMI) {
+		List<uvVertex[]> cgroup = new ArrayList<ObjModel.uvVertex[]>();
+		String gName = "Default";
+		for (int i=0; i<objMI.faces.size(); i++){
+			if (objMI.groups.containsKey(i)){
+				if (cgroup.size()>0){
+					groups.put(gName, cgroup);
+				}
+				cgroup = new ArrayList<ObjModel.uvVertex[]>();
+				gName = objMI.groups.get(i);
+			}
+			cgroup.add(objMI.faces.get(i));
+		}
+		groups.put(gName, cgroup);
+	}
+
+	public static ObjModel loadFromResource(String resName){
+		InputStream ris = ObjModel.class.getResourceAsStream(resName);
+		ObjModel objMI;
+		try {
+			objMI = new ObjModel(new ObjModelImporter(ris));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return objMI;
+	}
+
+	public void renderAll(){
+		for (String gn : groups.keySet()){
+			renderGroup(gn);
+		}
+	}
+
+	public void renderGroup(String gName){
+		if (groups.containsKey(gName)){
+			if (!cpldGroups.containsKey(gName)){
+				compileDisplayList(gName, 0);
+			}
+			GL11.glCallList(cpldGroups.get(gName));
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void compileDisplayList(String grp, float par1)
+	{
+		int dl = GLAllocation.generateDisplayLists(1);
+		GL11.glNewList(dl, GL11.GL_COMPILE);
+		Tessellator tes = Tessellator.instance;
 		
-		public List<Vector3> vertices;
-		public Map<Integer, String> groups;
-		public List<double[]> uvs;
-		public List<faceVert[]> faces;
-		
+		for (uvVertex[] face : groups.get(grp)){
+			tes.startDrawingQuads();
+			for (uvVertex vert : face){
+				if (vert.normal != null){
+					tes.setNormal((float)vert.normal.x, (float)vert.normal.y, (float)vert.normal.z);
+				}
+				tes.addVertexWithUV(vert.vertex.x, vert.vertex.y, vert.vertex.z, vert.uv.u, vert.uv.v);
+			}
+			tes.draw();
+		}
+
+		GL11.glEndList();
+		cpldGroups.put(grp, dl);
+	}
+
+	public static class ObjModelImporter {
+
+		public List<Vector3> vertices = new ArrayList<Vector3>();
+		public List<uvPair> uvs = new ArrayList<uvPair>();
+		public List<Vector3> normals = new ArrayList<Vector3>();
+		public List<uvVertex[]> faces = new ArrayList<uvVertex[]>();
+		public Map<Integer, String> groups = new HashMap<Integer, String>();
+
 		private int readInt(StreamTokenizer st) throws IOException {
 			if (st.nextToken() != st.TT_NUMBER)
 				throw new IOException("Failed to parse int");
 			return (int)st.nval;
 		}
-		
+
 		private double readDouble(StreamTokenizer st) throws IOException {
 			if (st.nextToken() != st.TT_NUMBER)
 				throw new IOException("Failed to parse double");
 			return st.nval;
 		}
-		
+
 		private String readString(StreamTokenizer st) throws IOException {
 			if (st.nextToken() != st.TT_WORD)
 				throw new IOException("Failed to parse String");
 			return st.sval;
 		}
-		
+
 		private void readLineEnd(StreamTokenizer st) throws IOException {
 			if (st.nextToken() != st.TT_EOL)
 				throw new IOException("EOL expected");
 		}
-		
+
+		private void readToEOL(StreamTokenizer st) throws IOException {
+			while (st.nextToken() != st.TT_EOL);
+		}
+
 		private void parseFace(StreamTokenizer st) throws IOException {
-			List<faceVert> fverts = new ArrayList<ObjModel.ObjModelImporter.faceVert>();
-			while (st.nextToken() != st.TT_EOL && st.ttype != st.TT_EOF){
+			List<uvVertex> fverts = new ArrayList<uvVertex>();
+			st.nextToken();
+			while (st.ttype != st.TT_EOL && st.ttype != st.TT_EOF){
 				if (st.ttype != st.TT_NUMBER)
 					throw new IOException("Failed to parse int");
 				
-				int vref = (int)st.nval;
+				System.out.println(st.nval);
+				
+				int vref = (int)st.nval-1;
 				if (st.nextToken() != '/')
 					throw new IOException("Failed to parse face");
 				
-				fverts.add(new faceVert(vref, readInt(st), 0));
+				int uvref = readInt(st)-1;
+				
+				int nref = -1;
+				if (st.nextToken() == '/'){
+					nref = readInt(st)-1;
+					st.nextToken();
+				}
+				fverts.add(new uvVertex(this, vref, uvref, nref));
 			}
-			faces.add((faceVert[])fverts.toArray());
+			if (fverts.size()<3 || fverts.size()>4){
+				throw new IllegalArgumentException("Face must have no less than 3 or no more than 4 vertices");
+			}
+			faces.add(fverts.toArray(new uvVertex[fverts.size()]));
 		}
-		
+
 		public ObjModelImporter(InputStream is) throws IOException {
 			Reader r = new BufferedReader(new InputStreamReader(is));
 			StreamTokenizer st = new StreamTokenizer(r);
@@ -66,46 +162,72 @@ public class ObjModel {
 			st.eolIsSignificant(true);
 			st.lowerCaseMode(false);
 			st.parseNumbers();
-			
+
 			while (st.nextToken() != st.TT_EOF){
+				System.out.println(st.sval);
 				if (st.ttype == st.TT_EOL){
 					continue;
 				} else if (st.ttype != st.TT_WORD) {
 					throw new IOException("Expected an OBJ type to start the line");
 				}
-				
+
 				// If only it could be assumed that everyone had Java 7...
-				if (st.sval == "v"){
+				if (st.sval.equals("v")){
 					Vector3 nv = new Vector3(readDouble(st), readDouble(st), readDouble(st));
 					vertices.add(nv);
 					readLineEnd(st);
 					continue;
-				} else if (st.sval == "vt") {
-					// TODO vt
+				} else if (st.sval.equals("vt")) {
+					uvs.add(new uvPair(readDouble(st), 1-readDouble(st)));
 					readLineEnd(st);
 					continue;
-				} else if (st.sval == "f") {
+				} else if (st.sval.equals("vn")){
+					Vector3 nv = new Vector3(readDouble(st), readDouble(st), readDouble(st));
+					normals.add(nv);
+					readLineEnd(st);
+					continue;
+				} else if (st.sval.equals("f")) {
 					parseFace(st);
-					readLineEnd(st);
+					//readLineEnd(st);
 					continue;
-				} else if (st.sval == "g") {
+				} else if (st.sval.equals("g")) {
 					groups.put(faces.size(), readString(st));
 					readLineEnd(st);
 					continue;
+				} else {
+					readToEOL(st);
 				}
 			}
 		}
+	}
+	
+	public static class uvPair {
+		public double u;
+		public double v;
+
+		public uvPair(double iu, double iv) {
+			u = iu;
+			v = iv;
+		}
+	}
+
+	public static class uvVertex {
+		public Vector3 vertex;
+		public uvPair uv;
+		public Vector3 normal;
+
+		public uvVertex(Vector3 ivert, uvPair iuv, Vector3 norm) {
+			vertex = ivert;
+			uv = iuv;
+			normal = norm;
+		}
 		
-		private static class faceVert {
-			public int vertRef;
-			public int uvRef;
-			public double normal;
-			
-			public faceVert(int vert, int uv, int norm) {
-				vertRef = vert;
-				uvRef = uv;
-				normal = norm;
-			}
+		public uvVertex(ObjModelImporter imp, int vref, int uvref, int nref) {
+			vertex = imp.vertices.get(vref);
+			if (uvref>-1)
+				uv = imp.uvs.get(uvref);
+			if (nref>-1)
+				normal = imp.normals.get(nref);
 		}
 	}
 }
