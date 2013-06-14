d7d4d8412911714293123e56adc8c29bccbee60f
Matchlighter <ml@matchlighter.net>
2013-06-13 22:49:00 -0600

    Try that.

--- src/minecraft/ml/core/MLCore.java
+++ src/minecraft/ml/core/MLCore.java
@@ -1,17 +1,19 @@
 package ml.core;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Map;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
 
 import ml.core.internal.PacketHandler;
-import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @Mod(modid="MLCore", name="MLCore")
@@ -21,6 +23,16 @@ public class MLCore implements IFMLLoadingPlugin {
 	
 	@Instance("MLCore")
 	public static MLCore instance;
+	
+	@Init
+	public void init(FMLInitializationEvent evt) {
+		try {
+			Field f = GuiScreen.class.getField("mc");
+			System.out.println(Modifier.isPublic(f.getModifiers()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public String[] getLibraryRequestClass() {
