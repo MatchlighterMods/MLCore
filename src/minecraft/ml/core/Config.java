1141ed26c3319609a6bc93455e791bbd9a0de20c
Matchlighter <ml@matchlighter.net>
2013-08-28 17:05:20 -0600

    Added some more cool stuff to Config

--- src/minecraft/ml/core/Config.java
+++ src/minecraft/ml/core/Config.java
@@ -16,6 +16,15 @@ import cpw.mods.fml.common.FMLLog;
 
 public abstract class Config {
 
+	/**
+	 * Property names will be determined by the field name if {@link Prop#inFileName()} is not set.<br/><br/>
+	 * 
+	 * If you prefix a field name with the camelCased category name and an '_', the prefix will be removed when saved to file.<br/>
+	 * <b>Ex:</b> "generalChild_property" will be saved as simply "property" if it is in the category "general.child"<br/>
+	 * In this scenario, "child_property" will also become just "property" in the config file; you can go as deep or challow into the category ancestory as you want.
+	 * 
+	 * @author Matchlighter
+	 */
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.FIELD)
 	public static @interface Prop {
@@ -24,9 +33,11 @@ public abstract class Config {
 		public String inFileName() default "";
 		
 		/**
-		 * If you rename a property, add this annotation with any old names.
-		 * The config will load the most recent name that it can find.
-		 * Will resave as the latest name and delete old names.
+		 * If you rename a property, add this annotation with any old names.<br/>
+		 * The config will load the most recent name that it can find.<br/>
+		 * Will resave as the latest name and delete old names.<br/><br/>
+		 * 
+		 * End the pattern with a . to take the current name (For easily changing categories)<br/><br/>
 		 * 
 		 * *Do NOT create a new property with the name of a renamed property. It will be deleted.
 		 */
@@ -57,14 +68,31 @@ public abstract class Config {
 				Prop ann = fld.getAnnotation(Prop.class);
 				if (ann != null){
 					Class type = fld.getType();
-					String propName = ann.inFileName().isEmpty() ? fld.getName() : ann.inFileName();
+					String fldName = fld.getName();
+					
+					String[] cats = ann.category().split("\\.");
+					for (int i=cats.length-1; i>=0; i--) {
+						String cccat = cats[i].toLowerCase();
+						for (int j=i+1; j<cats.length; j++) {
+							cccat += cats[j].substring(0, 1).toUpperCase() + cats[j].substring(1);
+						}
+						cccat+="_";
+						if (fldName.startsWith(cccat))
+							fldName = fldName.substring(cccat.length());
+					}
+					fldName = fldName.substring(0, 1).toLowerCase() + fldName.substring(1);
+					
+					String propName = ann.inFileName().isEmpty() ? fldName : ann.inFileName();
 					Property cProp = null;
 					
 					Property.Type forgeTyp = getForgeType(type);
 					
 					Renamed oldNames = fld.getAnnotation(Renamed.class);
 					if (oldNames != null)
-						for (String onm : oldNames.value()) {
+						for (int i=0; i<oldNames.value().length; i++) {
+							String onm = oldNames.value()[i];
+							
+							if (onm.endsWith(".")) onm += propName;
 							String[] path = onm.split("\\"+Configuration.CATEGORY_SPLITTER);
 							onm = path[path.length-1];
 							ConfigCategory cat = cfg.getCategory(path.length>1 ? StringUtils.join(Arrays.copyOf(path, path.length-1), ".") : ann.category());
