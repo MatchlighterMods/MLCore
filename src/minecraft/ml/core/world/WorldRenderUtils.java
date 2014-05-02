a10b7c3dc7b217c9619295b3737f6f4e22ca243b
Matchlighter <ml@matchlighter.net>
2014-05-02 12:00:01 -0600

    Fixed a few bugs

--- src/minecraft/ml/core/world/WorldRenderUtils.java
+++ src/minecraft/ml/core/world/WorldRenderUtils.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class WorldRenderUtils {
 	
 	private static RenderBlocks renderBlocks = new RenderBlocks();
-	private static EntityItem renderEnt;
+	private static EntityItem renderEnt = new EntityItem(null);
 	
 	private static boolean shouldSpreadItems = false;
 	private static RenderItem renderItem = new RenderItem() {
@@ -37,40 +37,44 @@ public class WorldRenderUtils {
 	};
 	
 	
-	static{
+	static {
 		renderItem.setRenderManager(RenderManager.instance);
-		renderEnt = new EntityItem(null);
 		renderEnt.hoverStart = 0F;
 	}
 	
 	public static void renderItemIntoWorldCenteredAt(ItemStack is, boolean renderIn3D, boolean spreadItems) {
 		shouldSpreadItems = spreadItems;
-		boolean isBlock = is.getItem() instanceof ItemBlock;
-		if (!renderIn3D){
-			GL11.glPushMatrix();
-			GL11.glScalef(0.03125F, 0.03125F, -0.0004F);
-			GL11.glTranslatef(8, 8, 0);
-			GL11.glRotatef(180F, 0, 0, 1.0F);
-
-			if (!ForgeHooksClient.renderInventoryItem(renderBlocks, RenderManager.instance.renderEngine, is, true, 0, 0F, 0F)) {
-				renderItem.renderItemIntoGUI(null, RenderManager.instance.renderEngine, is, 0, 0);
-			}
-			GL11.glPopMatrix();
-		} else {
-			IItemRenderer cstm = MinecraftForgeClient.getItemRenderer(is, ItemRenderType.ENTITY);
-			boolean renderAs3DBlock = cstm != null && cstm.shouldUseRenderHelper(ItemRenderType.ENTITY, is, ItemRendererHelper.BLOCK_3D);
-			if (isBlock || renderAs3DBlock){
-				if (renderAs3DBlock || RenderBlocks.renderItemIn3d(Block.blocksList[is.itemID].getRenderType())) {
-					GL11.glScalef(1.5F, 1.5F, 1.5F);
-					GL11.glRotatef(90F, 0, 1F, 0);
+		if (is != null) {
+			boolean isBlock = is.getItem() instanceof ItemBlock;
+			if (!renderIn3D){
+				GL11.glPushMatrix();
+				GL11.glScalef(0.03125F, 0.03125F, -0.0004F);
+				GL11.glTranslatef(8, 8, 0);
+				GL11.glRotatef(180F, 0, 0, 1.0F);
+	
+				if (!ForgeHooksClient.renderInventoryItem(renderBlocks, RenderManager.instance.renderEngine, is, true, 0, 0F, 0F)) {
+					renderItem.renderItemIntoGUI(null, RenderManager.instance.renderEngine, is, 0, 0);
+				}
+				GL11.glPopMatrix();
+			} else {
+				IItemRenderer cstm = MinecraftForgeClient.getItemRenderer(is, ItemRenderType.ENTITY);
+				boolean renderAs3DBlock = cstm != null && cstm.shouldUseRenderHelper(ItemRenderType.ENTITY, is, ItemRendererHelper.BLOCK_3D);
+				if (isBlock || renderAs3DBlock){
+					if (renderAs3DBlock || RenderBlocks.renderItemIn3d(Block.blocksList[is.itemID].getRenderType())) {
+						GL11.glScalef(1.5F, 1.5F, 1.5F);
+						GL11.glRotatef(90F, 0, 1F, 0);
+					} else {
+						GL11.glTranslatef(0F, -0.125F, 0F);
+					}
 				} else {
 					GL11.glTranslatef(0F, -0.125F, 0F);
 				}
-			} else {
-				GL11.glTranslatef(0F, -0.125F, 0F);
+				//This may happen when used from within an ItemRenderer. It should fix itself after one render loop.
+				if (RenderManager.instance.renderEngine != null) {
+					renderEnt.setEntityItemStack(is);
+					renderItem.doRenderItem(renderEnt, 0, 0, 0, 0, 0);
+				}
 			}
-			renderEnt.setEntityItemStack(is);
-			renderItem.doRenderItem(renderEnt, 0, 0, 0, 0, 0);
 		}
 	}
 	
