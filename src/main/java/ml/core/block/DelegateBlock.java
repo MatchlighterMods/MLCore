fa14899006e85e8e1e2e929a9e8ee4ba264d61e1
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-09-15 01:26:13 -0500

    fixed ForgeDirection import

--- src/main/java/ml/core/block/DelegateBlock.java
+++ src/main/java/ml/core/block/DelegateBlock.java
@@ -18,7 +18,7 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.IPlantable;
 import cpw.mods.fml.relauncher.Side;
