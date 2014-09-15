11f7bdd2c4ec5c8f19264daa2cef2c972b9973a9
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-09-15 01:19:59 -0500

    fixed ForgeDirection import

--- src/main/java/ml/core/block/BlockDelegator.java
+++ src/main/java/ml/core/block/BlockDelegator.java
@@ -26,7 +26,7 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.common.IPlantable;
 
 /**
