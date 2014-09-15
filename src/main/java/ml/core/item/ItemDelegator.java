24f8a5e650f426f27334cb6c70f0483b9d5e7d45
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-09-15 03:08:38 -0500

    fixed IIcon and IIconRegister imports

--- src/main/java/ml/core/item/ItemDelegator.java
+++ src/main/java/ml/core/item/ItemDelegator.java
@@ -2,14 +2,14 @@ package ml.core.item;
 
 import java.util.List;
 
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import com.google.common.collect.BiMap;
@@ -146,19 +146,19 @@ public class ItemDelegator <DCls extends DelegateItem> extends Item {
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public Icon getIconFromDamage(int par1) {
+	public IIcon getIconFromDamage(int par1) {
 		return subItem(par1).getIcon(par1);
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public Icon getIcon(ItemStack stack, int pass) {
+	public IIcon getIcon(ItemStack stack, int pass) {
 		return subItem(stack).getIcon(stack, pass);
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public Icon getIcon(ItemStack stack, int renderPass, EntityPlayer player, ItemStack usingItem, int useRemaining) {
+	public IIcon getIcon(ItemStack stack, int renderPass, EntityPlayer player, ItemStack usingItem, int useRemaining) {
 		return subItem(stack).getIconInHand(stack, renderPass, player, usingItem, useRemaining);
 	}
 	
@@ -169,7 +169,7 @@ public class ItemDelegator <DCls extends DelegateItem> extends Item {
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void registerIcons(IconRegister par1IconRegister) {
+	public void registerIcons(IIconRegister par1IconRegister) {
 		for (DCls sub : subItems.values()) sub.registerIcons(par1IconRegister);
 	}
 	
